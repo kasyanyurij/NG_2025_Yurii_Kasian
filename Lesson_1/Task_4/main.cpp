113129281ac5fcf35e0bb62143386d79c38003ce
kasyanyurij <kasyanyurij@gmail.com>
2025-10-27 21:08:54 +0200

    Update main.cpp

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -1,17 +1,25 @@
+#include <QCoreApplication>
 #include <iostream>
 #include <string>
 
-int main(){
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
     int k;
     char z='*';
     std::cout<<"max кількість зірочок:";
     std::cin>>k;
     if (k<0){
-            std::cout<<"error";
+        std::cout<<"error";
     }
     for (int i=0; i<=k; i++){
-        std::string fig(i, z);
-        std::cout << fig << std::endl;
+        std::string fig_t(i, z);
+        std::cout << fig_t << std::endl;
     }
-    return 0;
+    std::cout<<"\n";
+    std::string fig_p(k, z);
+    for (int j = 0; j < k; j++){
+            std::cout << fig_p << std::endl;
+    }
+    return a.exec();
 }
